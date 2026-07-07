67eb58464f1f06bd2adc8dd6eeb19621360d7bac
Oussama EL-BOUBEKRI <oussamaelboub@gmail.com>
2026-07-07 14:22:05 +0200

    Add: minimum-stack - submission-13

---
+++ Data Structures & Algorithms/minimum-stack/submission-13.py
@@ -0,0 +1,22 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.minimum = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if len(self.minimum) == 0 or val <= self.minimum[-1]:
+            self.minimum.append(val)
+
+    def pop(self) -> None:
+        v = self.stack.pop()
+        if v == self.minimum[-1]:
+            self.minimum.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        print(self.minimum)
+        return self.minimum[-1]
