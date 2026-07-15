6e30ca8e0af81cb9f3a90cffc9a9fda0484ebfe5
Oussama EL-BOUBEKRI <oussamaelboub@gmail.com>
2026-07-15 13:25:10 +0200

    Add: permutation-string - submission-0

---
+++ Data Structures & Algorithms/permutation-string/submission-0.py
@@ -0,0 +1,25 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s2) < len(s1):
+            return False
+        dic_s1 = {}
+        for k in range(len(s1)):
+            if s1[k] not in dic_s1.keys():
+                dic_s1[s1[k]] = 1
+            else:
+                dic_s1[s1[k]] += 1
+        print(dic_s1)
+        i,j = 0,len(s1)
+        while j <= len(s2):
+            dic = {}
+            for k in range(i,j):
+                if s2[k] not in dic.keys():
+                    dic[s2[k]] = 1
+                else:
+                    dic[s2[k]] += 1
+            if dic == dic_s1:
+                return True
+            else:
+                i+=1
+                j+=1
+        return False
